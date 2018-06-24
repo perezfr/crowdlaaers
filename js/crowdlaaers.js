9e7369269dcf43212c82d16cbedf7b0c1b0e9fe9
FranciscoPerez <franciscoperez@FranciscoPerezs-MacBook-Pro.local>
2018-06-24 15:49:14 -0600

    Looking at finding all tags

--- js/crowdlaaers.js
+++ js/crowdlaaers.js
@@ -5,79 +5,79 @@ $( document ).ready(function() {
   var syllabus = {
     august2016:{
       url:"http://www.commonsense.org/education/privacy/blog/digital-redlining-access-privacy",
-      summary:"In August 2016, Marginal Syllabus text-participants read and discussed Digital Redlining, Access, and Privacy, a blog post for Common Sense Education written by Chris Gilliard and Hugh Culik. The authors explain how “digital redlining” creates inequitable educational opportunities for learners."
+      summary:"Starting in August 2016, Marginal Syllabus text-participants read and discussed “Digital Redlining, Access, and Privacy,” a blog post for Common Sense Education written by Chris Gilliard and Hugh Culik. Chris and Hugh’s post discusses how “digital redlining” creates inequitable educational opportunities for learners."
     },
     september2016:{
       url:"http://dmlcentral.net/speculative-design-for-emergent-learning-taking-risks",
-      summary:""
+      summary:"Starting in September 2016, Marginal Syllabus text-participants read and discussed “Speculative Design for Emergent Learning: Taking Risks,” a blog post for DML Central by Mia Zamora. Mia’s post reflects upon her “Writing Race and Ethnicity” course and shares insight about her instructional decision-making and co-design with students."
     },
     october2016:{
       url:"http://marginalsyllab.us/wp-content/uploads/2016/08/PWFlow-Intro.pdf",
-      summary:""
+      summary:"Starting in October 2016, Marginal Syllabus text-participants read and discussed “What It Means to Pose, Wobble, and Flow,” the introductory chapter of Pose, Wobble, Flow: A Culturally Proactive Approach to Literacy Instruction by Antero Garcia and Cindy O’Donnell-Allen. Antero and Cindy’s chapter introduces a yoga metaphor to discuss teacher learning about culturally relevant literacy pedagogy and student literacy practices."
     },
     november2016:{
       url:"https://helenbeetham.com/2016/11/14/ed-tech-and-the-circus-of-unreason",
-      summary:""
+      summary:"Starting in November 2016, Marginal Syllabus text-participants read and discussed “Ed Tech and the Circus of Unreason,” a blog post by Helen Beetham. Helen’s post critically analyzes the state of educational technology, with a focus on higher education contexts, and in response suggests educator responsibilities that include a focus on addressing educational inequalities."
     },
     january2016:{
       url:"http://marginalsyllab.us/the-school-and-social-progress-by-john-dewey",
-      summary:""
+      summary:"Starting in January 2017, Marginal Syllabus text-participants read and discussed “The School and Social Progress,” the first chapter from John Dewey’s seminal book The School and Society. This conversation was organized in collaboration with Christina Cantrill and teacher education courses at Arcadia University."
     },
     february2016:{
       url:"http://marginalsyllab.us/preface-to-research-writing-rewired-lessons-that-ground-students-digital-learning-by-dawn-reed-and-troy-hicks/",
-      summary:""
+      summary:"Starting in February 2017, Marginal Syllabus text-participants read and discussed “Reading, Writing, and Inquiry With Adolescents,” the preface to Research Writing Rewired: Lessons that Ground Digital Learning by Dawn Reed and Troy Hicks. Dawn and Troy’s chapter shares principles they employ for research writing, discuss the ubiquity of digital tools for writing and publication, and comment upon the internet as a site for inquiry and research."
     },
     march2016:{
       url:"https://www.colorlines.com/articles/how-can-white-teachers-do-better-urban-kids-color",
-      summary:""
+      summary:"Starting in March 2017, Marginal Syllabus text-participants read and discussed “How Can White Teachers Do Better by Urban Kids of Color?,” an excerpt from For White Folks Who Teach in the Hood…and the Rest of Y’all Too: Reality Pedagogy and Urban Education by Christopher Emdin. Christopher’s excerpt discusses race in the classroom by contrasting the voices of students of color with those of white educators, while also sharing reflections about pedagogy, asset-based education, and cultural relevance."
     },
     april2016:{
       url:"http://educatorinnovator.org/between-storytelling-and-surveillance-the-precarious-public-of-american-muslim-youth/",
-      summary:""
+      summary:"Starting in April 2017, and in collaboration with Educator Innovator, Marginal Syllabus text-participants read and discussed “Between Storytelling and Surveillance: The Precarious Public of American Muslim Youth,” a chapter by Sangita Shresthova from By Any Media Necessary (co-authored by Henry Jenkins, Sangita Shresthova, Liana Gamber-Thompson, Neta Kligler-Vilenchik, and Arely Zimmerman). Sangita’s chapter addresses the ways in which American Muslim youth experience islamophobia online, and also how they endure the criticism of older community members who may take issue with some youth’s desire to have expressive and creative online identities."
     },
     may2016:{
       url:"https://via.hypothes.is/http://educatorinnovator.org/wp-content/uploads/2017/05/LaMay-Ch5.pdf",
-      summary:""
+      summary:"Starting in May 2017, and in collaboration with Educator Innovator, Marginal Syllabus text-participants read and discussed “Revising Narrative Truth,” a chapter from Personal Narrative, Revised by Bronwyn Clare LaMay. Bronwyn’s chapter shares a challenging and inspiring story about how to develop inclusive classroom communities that nurture student risk-taking, expression, and learning."
     },
     october2017:{
       url:"https://educatorinnovator.org/wp-content/uploads/2017/10/Critical-Literacy-And-Our-Students-Lives.pdf",
-      summary:""
+      summary:"Starting in October 2017, Marginal Syllabus text-participants read and discussed “How Young Activists Deploy Digital Tools for Social Change,” a blog post for DML Central by Henry Jenkins. Henry’s post, part of the Writing Our Civic Futures syllabus, discusses how youth from different cultural contexts participate in digital politics and contribute to social movements."
     },
     november2017:{
       url:"https://educatorinnovator.org/wp-content/uploads/2017/10/RRE_Chapter-6_Civic-Participation-Remiagined_0091732X17690121.pdf",
-      summary:""
+      summary:"Starting in November 2017, Marginal Syllabus text-participants read and discussed “Civic Participation Reimagined: Youth Interrogation and Innovation in the Multimodal Public Sphere,” an article in Review of Research in Education by Nicole Mirra and Antero Garcia. Nicole and Antero’s article, part of the Writing Our Civic Futures syllabus, challenges dominant narratives about the civic disengagement of youth from marginalized communities and develops a new conceptual model for civic learning and engagement that pushes past participation into the realms of interrogation and innovation."
     },
     december2017:{
       url:"https://educatorinnovator.org/wp-content/uploads/2017/10/Critical-Literacy-And-Our-Students-Lives.pdf",
-      summary:""
+      summary:"Starting in December 2017, Marginal Syllabus text-participants read and discussed “Critical Literacy and Our Students’ Lives,” an article in Voices From the Middle by Linda Christensen. Linda’s article, part of the Writing Our Civic Futures syllabus, shares pedagogical reflections on the importance of critical literacy, with an emphasis on developing curricula and lessons that center students’ lives and interests."
     },
     january2017:{
       url:"https://educatorinnovator.org/wp-content/uploads/2017/12/OurDeclaration_PG31-35.pdf",
-      summary:""
+      summary:"Starting in January 2018, Marginal Syllabus text-participants read and discussed “Night Teaching,” the second chapter from Our Declaration: A Reading of the Declaration of Independence in Defense of Equality by Danielle Allen. Daniell’s chapter, part of the Writing Our Civic Futures syllabus, recalls her experiences teaching the Declaration of Independence to night students at the University of Chicago, and includes incisive reflections about political equality, freedom, and the power of language."
     },
     february2017:{
       url:"https://educatorinnovator.org/wp-content/uploads/2018/02/Educating-for-Democracy-in-a-Partisan-Age.pdf",
-      summary:""
+      summary:"Starting in February 2018, Marginal Syllabus text-participants read and discussed “Educating for Democracy in a Partisan Age: Confronting the Challenges of Motivated Reasoning and Misinformation,” an article in American Educational Research Journal by Joseph Kahne and Benjamin Bowyer. Joseph and Benjamin’s article, part of the Writing Our Civic Futures syllabus, presents a study that Investigated youth judgments of the accuracy of truth claims tied to controversial public issues. The authors found that youth’s political knowledge did not improve judgments of accuracy but that media literacy education did."
     },
     march2017:{
       url:"https://educatorinnovator.org/wp-content/uploads/2018/02/The-Stories-They-Tell-.pdf",
-      summary:""
+      summary:"Starting in March 2018, Marginal Syllabus text-participants read and discussed “The Stories They Tell: Mainstream Media, Pedagogies of Healing, and Critical Media Literacy,” an article in English Education by April Baker-Bell, Raven Jones Stanbrough, and Sakeena Everett. April, Raven, and Sakeena’s article, part of the Writing Our Civic Futures syllabus, discusses how pedagogies of healing and critical media literacy are necessary in the wake of racial violence when mainstream media stigmatize, characterize, and marginalize Black youth. The article includes sample lesson plans and a discussion of how English educators have a responsibility to raise awareness about racial injustice."
     },
     april2017:{
       url:"https://educatorinnovator.org/wp-content/uploads/2018/03/Educating-Youth-for-Online-Civic-and-Political-Dialogue_-A-Conceptual-Framework-for-the-Digital-Age-_-Journal-of-Digital-and-Media-Literacy.pd",
-      summary:""
+      summary:"Starting in April 2018, Marginal Syllabus text-participants read and discussed “Educating Youth for Online Civic and Political Dialogue: A Conceptual Framework for the Digital Age,” an article in the Journal of Digital and Media Literacy by Erica Hodgin. Erica’s article, part of the Writing Our Civic Futures syllabus, reports upon four high school teachers’ work on a participatory academic platform and details five stages of opportunity that built young people’s capacity for civic voice and influence."
     },
     may2017:{
       url:"https://educatorinnovator.org/wp-content/uploads/2018/03/zemelman_websample.pdf",
-      summary:""
+      summary:"Starting in May 2018, Marginal Syllabus text-participants read and discussed the introduction of From Inquiry to Action: Civic Engagement with Project-Based Learning in All Content Areas by Steven Zemelman. Steve’s chapter, part of the Writing Our Civic Futures syllabus, provides practice-based examples and strategies to engage students in social action projects with a focus on amplifying student choice, strengthening engagement, and addressing injustices in the community."
     },
     june2017:{
       url:"http://marginalsyllab.us/wp-content/uploads/2016/08/PWFlow-Intro.pdf",
-      summary:""
+      summary:"Starting in June 2018, Marginal Syllabus text-participants read and discussed “The Next Decade of Digital Writing,” an article in Voices from the Middle by Troy Hicks. Troy’s article, an interest-driven addition to the Writing Our Civic Futures syllabus, argues that educators need to provide intellectual and emotional space for students to explore new ideas, gather their own evidence, and present academic arguments through media other than just the printed word."
     }
   }
 
-  $("#conversation_summary").html(syllabus['august2016']['summary']);
+  $("#conversation_summary").html(syllabus['june2017']['summary']);
 
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
@@ -87,7 +87,7 @@ $( document ).ready(function() {
     }
   };
 
-  xhttp.open("GET", "https://hypothes.is/api/search?url=" + syllabus['august2016']['url'] + "&limit=200", true);
+  xhttp.open("GET", "https://hypothes.is/api/search?url=" + syllabus['june2017']['url'] + "&limit=200", true);
   xhttp.setRequestHeader("Content-type", "application/json");
   xhttp.send();
 
@@ -174,7 +174,8 @@ $( document ).ready(function() {
     var bar_graph = new google.visualization.ColumnChart(document.getElementById('graph'));
     var calendar = new google.visualization.Calendar(document.getElementById('graph'));
     var opts = {
-      width: '100%', height: '100%', page: 'enable', pageSize: 25, legend: { position: 'none' }
+      width: '100%', height: '100%', page: 'enable', pageSize: 25, legend: { position: 'none' },
+      vAxis: { format: '#' }
     };
     var view = new google.visualization.DataView(data);
     view.hideColumns([3,4]);
