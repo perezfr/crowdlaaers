51cd76f3b0bcafbedd3b4d74410d741073dbc926
perezfr <franciscoperez@ip-172-16-9-54.ec2.internal>
2019-02-13 10:29:58 -0700

    added r2l project files

--- js/crowdlaaers.js
+++ js/crowdlaaers.js
@@ -507,6 +507,14 @@ $( document ).ready(function() {
     });
   }
 
+  if (startURL.href.includes("r2l.html")){
+    $("#conversation_summary").html(syllabus['active']['summary']);
+    params.url = syllabus['active']['url'];
+    google.charts.setOnLoadCallback(function() { //waits for graph lib to load before drawing
+      hlib.hApiSearch(params, processSearchResults, '');
+    });
+  }
+
   //Share button adds the url from the search bar as a parameter to the 
   //crowdlaaers search url.
   $( "#urlShare" ).click(function() {
