469f4c83acb31dded2cbe2f1b8d6d06d70a0bcd4
tiberious <tiberious@iMac.local>
2019-11-10 12:27:09 -0700

    changed active url to the last month to give the all option time to load

---
+++ projects/syl/marginalsyllabus-2017-18.js
@@ -0,0 +1,42 @@
+  var syllabus = {
+    active:{
+      url:"http://www.ncte.org/library/NCTEFiles/Resources/Journals/VM/0254-may2018/VM0254Leading.pdf",
+      summary:""
+    },
+    october2017:{
+      url:"http://educatorinnovator.org/how-young-activists-deploy-digital-tools-for-social-change",
+      summary:"Starting in October 2017, Marginal Syllabus text-participants read and discussed “How Young Activists Deploy Digital Tools for Social Change,” a blog post for DML Central by Henry Jenkins. Henry’s post, part of the Writing Our Civic Futures syllabus, discusses how youth from different cultural contexts participate in digital politics and contribute to social movements."
+    },
+    november2017:{
+      url:"https://educatorinnovator.org/wp-content/uploads/2017/10/RRE_Chapter-6_Civic-Participation-Remiagined_0091732X17690121.pdf",
+      summary:"Starting in November 2017, Marginal Syllabus text-participants read and discussed “Civic Participation Reimagined: Youth Interrogation and Innovation in the Multimodal Public Sphere,” an article in Review of Research in Education by Nicole Mirra and Antero Garcia. Nicole and Antero’s article, part of the Writing Our Civic Futures syllabus, challenges dominant narratives about the civic disengagement of youth from marginalized communities and develops a new conceptual model for civic learning and engagement that pushes past participation into the realms of interrogation and innovation."
+    },
+    december2017:{
+      url:"https://educatorinnovator.org/wp-content/uploads/2017/10/Critical-Literacy-And-Our-Students-Lives.pdf",
+      summary:"Starting in December 2017, Marginal Syllabus text-participants read and discussed “Critical Literacy and Our Students’ Lives,” an article in Voices From the Middle by Linda Christensen. Linda’s article, part of the Writing Our Civic Futures syllabus, shares pedagogical reflections on the importance of critical literacy, with an emphasis on developing curricula and lessons that center students’ lives and interests."
+    },
+    january2017:{
+      url:"https://educatorinnovator.org/wp-content/uploads/2017/12/OurDeclaration_PG31-35.pdf",
+      summary:"Starting in January 2018, Marginal Syllabus text-participants read and discussed “Night Teaching,” the second chapter from Our Declaration: A Reading of the Declaration of Independence in Defense of Equality by Danielle Allen. Daniell’s chapter, part of the Writing Our Civic Futures syllabus, recalls her experiences teaching the Declaration of Independence to night students at the University of Chicago, and includes incisive reflections about political equality, freedom, and the power of language."
+    },
+    february2017:{
+      url:"https://educatorinnovator.org/wp-content/uploads/2018/02/Educating-for-Democracy-in-a-Partisan-Age.pdf.pdf",
+      summary:"Starting in February 2018, Marginal Syllabus text-participants read and discussed “Educating for Democracy in a Partisan Age: Confronting the Challenges of Motivated Reasoning and Misinformation,” an article in American Educational Research Journal by Joseph Kahne and Benjamin Bowyer. Joseph and Benjamin’s article, part of the Writing Our Civic Futures syllabus, presents a study that Investigated youth judgments of the accuracy of truth claims tied to controversial public issues. The authors found that youth’s political knowledge did not improve judgments of accuracy but that media literacy education did."
+    },
+    march2017:{
+      url:"https://educatorinnovator.org/wp-content/uploads/2018/02/The-Stories-They-Tell-.pdf",
+      summary:"Starting in March 2018, Marginal Syllabus text-participants read and discussed “The Stories They Tell: Mainstream Media, Pedagogies of Healing, and Critical Media Literacy,” an article in English Education by April Baker-Bell, Raven Jones Stanbrough, and Sakeena Everett. April, Raven, and Sakeena’s article, part of the Writing Our Civic Futures syllabus, discusses how pedagogies of healing and critical media literacy are necessary in the wake of racial violence when mainstream media stigmatize, characterize, and marginalize Black youth. The article includes sample lesson plans and a discussion of how English educators have a responsibility to raise awareness about racial injustice."
+    },
+    april2017:{
+      url:"https://educatorinnovator.org/wp-content/uploads/2018/03/Educating-Youth-for-Online-Civic-and-Political-Dialogue_-A-Conceptual-Framework-for-the-Digital-Age-_-Journal-of-Digital-and-Media-Literacy.pdf",
+      summary:"Starting in April 2018, Marginal Syllabus text-participants read and discussed “Educating Youth for Online Civic and Political Dialogue: A Conceptual Framework for the Digital Age,” an article in the Journal of Digital and Media Literacy by Erica Hodgin. Erica’s article, part of the Writing Our Civic Futures syllabus, reports upon four high school teachers’ work on a participatory academic platform and details five stages of opportunity that built young people’s capacity for civic voice and influence."
+    },
+    may2017:{
+      url:"https://educatorinnovator.org/wp-content/uploads/2018/03/zemelman_websample.pdf",
+      summary:"Starting in May 2018, Marginal Syllabus text-participants read and discussed the introduction of From Inquiry to Action: Civic Engagement with Project-Based Learning in All Content Areas by Steven Zemelman. Steve’s chapter, part of the Writing Our Civic Futures syllabus, provides practice-based examples and strategies to engage students in social action projects with a focus on amplifying student choice, strengthening engagement, and addressing injustices in the community."
+    },
+    june2017:{
+      url:"http://www.ncte.org/library/NCTEFiles/Resources/Journals/VM/0254-may2018/VM0254Leading.pdf",
+      summary:"Starting in June 2018, Marginal Syllabus text-participants read and discussed “The Next Decade of Digital Writing,” an article in Voices from the Middle by Troy Hicks. Troy’s article, an interest-driven addition to the Writing Our Civic Futures syllabus, argues that educators need to provide intellectual and emotional space for students to explore new ideas, gather their own evidence, and present academic arguments through media other than just the printed word."
+    }
+  }
---
+++ projects/syl/marginalsyllabus-summer2019.js
@@ -0,0 +1,26 @@
+  var syllabus = {
+    active:{
+      url:"https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Baker-Doyle-2018.pdf",
+      summary:""
+    },
+    june2018:{
+      url:"https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/EJ_EvenCinderella.pdf",
+      summary:"Starting in June 2019, Marginal Syllabus text-participants read and discussed 'Even Cinderella Is White: (Re)Centering Black Girls’ Voices as Literacies of Resistance' an article in English Journal by Jemimah Young, Marquita Foster, and Dorothy Hines. The article, part of the Literacy, Equity + Remarkable Notes = LEARN syllabus, describes how counter fairy tales can (re)center the voices of Black girls as literacies of resistance in English education."
+    },
+    watulaketal2018:{
+      url:"https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Watulak-et-al-2018.pdf",
+      summary:"'Connected Teaching and Learning in K-16+ Contexts: An Annotated Bibliography' by Sarah Lohnes Watulak and colleagues was read and annotated during the Summer 2019 Connected Learning in Teacher Education Marginal Syllabus. Read all annotations in context: <a target='_blank' href='https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Watulak-et-al-2018.pdf'>https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Watulak-et-al-2018.pdf</a>"
+    },
+    hsieh2018:{
+      url:"https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Hsieh-2018.pdf",
+      summary:"'This Is How We Do It: Authentic and Strategic Technology Use by Novice English Teachers' by Betina Hsieh was read and annotated during the Summer 2019 Connected Learning in Teacher Education Marginal Syllabus. Read all annotations in context: <a target='_blank' href='https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Hsieh-2018.pdf'>https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Hsieh-2018.pdf</a>"
+    },
+    westpuckettetal2018:{
+      url:"https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/West-Puckett-et-al-2018.pdf",
+      summary:"'The Fallacies of Open: Participatory Design, Infrastructuring, and the Pursuit of Radical Possibility,' by Stephanie West-Puckett and colleagues was read and annotated during the Summer 2019 Connected Learning in Teacher Education Marginal Syllabus. Read all annotations in context: <a target='_blank' href='https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/West-Puckett-et-al-2018.pdf'>https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/West-Puckett-et-al-2018.pdf</a>"
+    },
+    bakerdoyle2018:{
+      url:"https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Baker-Doyle-2018.pdf",
+      summary:"'I, Pseudocoder: Reflections of a Literacy Teacher-Educator on Teaching Coding as Critical Literacy,' by Kira Baker-Doyle was read and annotated during the Summer 2019 Connected Learning in Teacher Education Marginal Syllabus. Read all annotations in context: <a target='_blank' href='https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Baker-Doyle-2018.pdf'>https://via.hypothes.is/https://educatorinnovator.org/wp-content/uploads/2019/05/Baker-Doyle-2018.pdf</a>"
+    }
+  }
